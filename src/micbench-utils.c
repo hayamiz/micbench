a614f3a8627eabbe72a7d8ad825212f1961601a9
Yuto Hayamizu <y.hayamizu@gmail.com>
2011-07-30 13:17:19 +0900

    moved affinity related code to utils

--- src/micbench-utils.c
+++ src/micbench-utils.c
@@ -1,6 +1,80 @@
 
 #include "micbench-utils.h"
 
+mb_affinity_t *
+mb_make_affinity(void)
+{
+    mb_affinity_t *affinity;
+
+    affinity = malloc(sizeof(mb_affinity_t));
+    affinity->tid = -1;
+    CPU_ZERO(&affinity->cpumask);
+    affinity->nodemask = 0;
+    affinity->optarg = NULL;
+
+    return affinity;
+}
+
+void
+mb_free_affinity(mb_affinity_t *affinity)
+{
+    if (affinity->optarg != NULL)
+        free(affinity->optarg);
+    free(affinity);
+}
+
+mb_affinity_t *
+mb_parse_affinity(mb_affinity_t *ret, const char *optarg)
+{
+    const char *str;
+    char *endptr;
+    int idx;
+
+    str = optarg;
+
+    if (ret == NULL){
+        ret = mb_make_affinity();
+    }
+
+    // parse thread id
+    ret->tid = strtol(str, &endptr, 10);
+    if (endptr == optarg) return NULL;
+    if (*endptr != ':') return NULL;
+
+    // parse cpu mask
+    CPU_ZERO(&ret->cpumask);
+    for(str = endptr + 1, idx = 0; *str != ':'; idx++, str++) {
+        if (*str == '\0') return NULL;
+        switch(*str){
+        case '1':
+            CPU_SET(idx, &ret->cpumask);
+            break;
+        case '0':
+            // do nothing
+            break;
+        default:
+            return NULL;
+        }
+    }
+
+    // parse node mask
+    ret->nodemask = 0;
+    for(str += 1, idx = 0; *str != '\0'; idx++, str++) {
+        switch(*str){
+        case '1':
+            ret->nodemask |= (1 << idx);
+            break;
+        case '0':
+            // do nothing
+            break;
+        default:
+            return NULL;
+        }
+    }
+
+    return ret;
+}
+
 double
 mb_elapsed_time_from(struct timeval *tv)
 {
--- src/micbench-utils.h
+++ src/micbench-utils.h
@@ -1,9 +1,22 @@
 #ifndef MICBENCH_UTILS_H
 #define MICBENCH_UTILS_H
 
+#define _GNU_SOURCE 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <sched.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+typedef struct {
+    pid_t tid;
+    cpu_set_t cpumask;
+    unsigned long nodemask;
+    char *optarg; // must be free-able pointer on discarding this struct
+} mb_affinity_t;
+
 
 // macros for struct timeval
 #define TV2LONG(tv)	(((long) (tv).tv_sec) * 1000000L + (tv).tv_usec)
@@ -17,6 +30,22 @@
             fprintf(stderr, " @%s:%d\n", __FILE__, __LINE__);   \
         }                                                       \
     }
+
+mb_affinity_t *mb_make_affinity(void);
+void           mb_free_affinity(mb_affinity_t *affinity);
+
+/**
+ * mb_parse_affinity:
+ * @ret: A pointer to the location where the result will be stored. If NULL, the result is stored in newly malloc-ed location.
+ * @optarg: A string passed as an option argument.
+ *
+ * This function parses an option argument of affinity and returns a
+ * pointer to the result. If @ret is not NULL, the result will be
+ * stored in @ret and @ret will be returned. If @ret is NULL, the
+ * result will be stored in newly malloc-ed location.
+ * If some error happen, NULL is returned.
+ */
+mb_affinity_t *mb_parse_affinity(mb_affinity_t *ret, const char *optarg);
 
 double mb_elapsed_time_from(struct timeval *tv);
 
