95632ee34d45fb931cd04dd114868221c138084c
Yuto Hayamizu <y.hayamizu@gmail.com>
2017-06-26 21:13:38 +0900

    removed unused script

--- disable-turboboost.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NUM_CPU=$(ls -d /sys/devices/system/cpu/*/cpufreq|wc -l)
-
-for i in $(seq $((NUM_CPU-1))); do
-    echo cpufreq-set -c $i -g performance
-    cpufreq-set -c $i -g performance
-done
